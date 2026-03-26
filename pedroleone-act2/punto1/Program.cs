badeabd39fbaeb9723748b1b08a91a3dc69a9cf1
P3DQL <pedroleone.hall@gmail.com>
2026-03-26 18:42:27 -0300

    Add files via upload

---
+++ pedroleone-act2/punto1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        /*1. Realizar un programa que lea por teclado dos números, 
+         * si el primero es mayor al segundo informar su suma y diferencia, 
+         * en caso contrario informar el producto y la división del primero respecto al segundo.
+        */
+        double a = Convert.ToDouble(Console.ReadLine());
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        if (a > b)
+        {
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+        }
+        else
+        {
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+        }
+    }
+}
---
+++ pedroleone-act2/punto2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /* Se ingresan seis notas de un alumno,
+         si el promedio es mayor o igual a siete mostrar un mensaje "Promocionado"
+        */
+        double suma = 0;
+
+        for (int i = 0; i < 6; i++)
+        {
+            suma += Convert.ToDouble(Console.ReadLine());
+        }
+
+        double promedio = suma / 6;
+
+        if (promedio >= 7)
+        {
+            Console.WriteLine("Promocionado");
+        }
+    }
+}
