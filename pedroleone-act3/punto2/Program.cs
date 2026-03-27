a4c7d2cd3cb8f3fd9ed0a5af91e73d304ca8b259
P3DQL <pedroleone.hall@gmail.com>
2026-03-28 01:50:17 +0400

    Add files via upload

---
+++ pedroleone-act3/punto2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        /*2. Se ingresan tres valores por teclado, 
+         * si todos son iguales se imprime la suma del primero con el segundo y 
+         * a este resultado se lo multiplica por el tercero.
+         */
+        string linea;
+        int a, b, c;
+
+        Console.Write("Ingrese el primer valor: ");
+        linea = Console.ReadLine();
+        a = int.Parse(linea);
+
+        Console.Write("Ingrese el segundo valor: ");
+        linea = Console.ReadLine();
+        b = int.Parse(linea);
+
+        Console.Write("Ingrese el tercer valor: ");
+        linea = Console.ReadLine();
+        c = int.Parse(linea);
+
+        if (a == b && b == c)
+        {
+            int resultado = (a + b) * c;
+            Console.WriteLine("Resultado: " + resultado);
+        }
+
+        Console.ReadKey();
+    }
+}
