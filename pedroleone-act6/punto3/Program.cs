9a28385ca7c0ab7c146d6368f47328b79f6a994b
P3DQL <pedroleone.hall@gmail.com>
2026-04-09 09:21:57 -0300

    Update Program.cs

--- pedroleone-act6/punto3/Program.cs
+++ pedroleone-act6/punto3/Program.cs
@@ -2,6 +2,11 @@
 
 class Empleado
 {
+    /* 3. Confeccionar una clase que represente un empleado. Definir como atributos su
+        nombre y su sueldo. Confeccionar los métodos para la carga, otro para imprimir
+        sus datos y por último uno que imprima un mensaje si debe pagar impuestos
+        (si el sueldo supera a 3000).
+    */
     private string nombre;
     private double sueldo;
 
