ca716699ab9ab0b10c54dfd289d1fe7fdbd83b8b
P3DQL <pedroleone.hall@gmail.com>
2026-04-20 19:56:13 -0300

    Implement accumulation and counting logic in Program.cs
    Refactor program to calculate totals and counts for vector elements.

--- pedroleone-act7/punto1/Program.cs
+++ pedroleone-act7/punto1/Program.cs
@@ -1,61 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace _7._1
 {
-    class Program
-    {
-        /*1. Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
+    /*1. Desarrollar un programa que permita ingresar un vector de 8 elementos, e informe:
         El valor acumulado de todos los elementos del vector.
         El valor acumulado de los elementos del vector que sean mayores a 36.
         Cantidad de valores mayores a 50.
-        */
+     */
+    class Program
+    {
         private int[] valores;
+
         public void Cargar()
         {
             valores = new int[8];
             for (int i = 0; i < 8; i++)
             {
                 Console.Write("Ingrese un valor: ");
-                string ingresado;
-                ingresado = Console.ReadLine();
-                valores[i] = int.Parse(ingresado);
+                valores[i] = int.Parse(Console.ReadLine());
             }
         }
+
         public void verify_and_print()
         {
-            Console.Write("Elementos del vector: ");
+            int sumaTotal = 0;
+            int sumaMayores36 = 0;
+            int cantidadMayores50 = 0;
+
             for (int i = 0; i < 8; i++)
             {
-                Console.Write(valores[i] + " ");
+                sumaTotal += valores[i];
+
+                if (valores[i] > 36)
+                    sumaMayores36 += valores[i];
+
+                if (valores[i] > 50)
+                    cantidadMayores50++;
             }
-            Console.WriteLine();
-            Console.Write("Elementos del vector mayores a 36: ");
-            foreach (int f in valores)
-            {
-                if (f > 36)
-                {
-                    Console.Write(f + " ");
-                }
-                else
-                {
-                }
-            }
-            Console.WriteLine();
-            Console.Write("Elementos del vector mayores a 50: ");
-            foreach (int x in valores)
-            {
-                if (x > 50)
-                {
-                    Console.Write(x + " ");
-                }
-            }
-            Console.WriteLine();
+
+            Console.WriteLine("Suma total: " + sumaTotal);
+            Console.WriteLine("Suma mayores a 36: " + sumaMayores36);
+            Console.WriteLine("Cantidad mayores a 50: " + cantidadMayores50);
+
             Console.ReadKey();
         }
+
         static void Main(string[] args)
         {
             Program p = new Program();
