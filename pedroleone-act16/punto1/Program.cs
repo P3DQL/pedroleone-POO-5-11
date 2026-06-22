ce14cfd7d6bd7eea29dba78cb32a3e4519272aa3
P3DQL <pedroleone.hall@gmail.com>
2026-06-22 19:09:00 -0300

    Update Program.cs
    Updated the Program class to allow user input for Persona and Empleado attributes.

--- pedroleone-act16/punto1/Program.cs
+++ pedroleone-act16/punto1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+
 /*1. Confeccionar una clase Persona que tenga como atributos el nombre y la
 edad (definir las propiedades para poder acceder a dichos atributos). Definir
 un método para imprimirlos. Plantear una segunda clase Empleado que
@@ -7,6 +8,7 @@ método para imprimir su sueldo. Definir un objeto de la clase Persona y
 llamar a sus métodos y propiedades. También crear un objeto de la clase
 Empleado y llamar a sus métodos y propiedades.
 */
+
 class Persona
 {
     private string nombre;
@@ -52,16 +54,26 @@ class Program
     static void Main()
     {
         Persona persona = new Persona();
-        persona.Nombre = "Juan";
-        persona.Edad = 25;
 
-        Console.WriteLine("DATOS PERSONA");
+        Console.Write("Ingrese nombre de la persona: ");
+        persona.Nombre = Console.ReadLine();
+
+        Console.Write("Ingrese edad de la persona: ");
+        persona.Edad = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("\nDATOS PERSONA");
         persona.Imprimir();
 
         Empleado empleado = new Empleado();
-        empleado.Nombre = "Ana";
-        empleado.Edad = 30;
-        empleado.Sueldo = 250000;
+
+        Console.Write("\nIngrese nombre del empleado: ");
+        empleado.Nombre = Console.ReadLine();
+
+        Console.Write("Ingrese edad del empleado: ");
+        empleado.Edad = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese sueldo del empleado: ");
+        empleado.Sueldo = double.Parse(Console.ReadLine());
 
         Console.WriteLine("\nDATOS EMPLEADO");
         empleado.Imprimir();
